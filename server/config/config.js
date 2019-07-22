346db56dad2098318557f0b99f6ad0b566cb7047
germanheinz <heinz.german@gmail.com>
2019-07-22 18:14:55 -0300

    Json Web Token - Login

--- server/config/config.js
+++ server/config/config.js
@@ -9,7 +9,9 @@ process.env.PORT = process.env.PORT || 3000;
 // ============================
 process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 
+process.env.CADUCIDAD_TOKEN = 60 * 20 * 24 * 20;
 
+process.env.SEED = process.env.SEED || 'este-es-el-seed';
 // ============================
 //  Base de datos
 // ============================
---
+++ server/middlewares/autenticacion.js
@@ -0,0 +1,45 @@
+const jwt = require('jsonwebtoken');
+
+//verificar token
+let verificaToken = (req, res, next) => {
+
+    let token = req.get('token');
+
+    jwt.verify(token, process.env.SEED, (err, decoded) => {
+
+        if (err) {
+            return res.status(401).json({
+                ok: false,
+                err: {
+                    message: 'Token no valido'
+                }
+            })
+        }
+
+        req.usuario = decoded.usuario;
+        next();
+
+    });
+
+};
+
+//verificar Admin Role
+let verificaRole = (req, res, next) => {
+
+    let usuario = req.usuario;
+
+    if (usuario.role === 'ADMIN_ROLE') {
+        next();
+    } else {
+        res.json({
+            ok: false,
+            err: {
+                message: 'El usuario no es Administrador'
+            }
+        });
+    }
+
+}
+
+
+module.exports = { verificaToken, verificaRole };
